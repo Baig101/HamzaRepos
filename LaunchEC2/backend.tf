2d67ce53a983628aacb438b68144946f42cf42d1
Baig101 <Muhammad.baig101@gmail.com>
2024-04-28 21:58:00 -0400

    updated files

--- LaunchEC2/backend.tf
+++ LaunchEC2/backend.tf
@@ -1,1 +1,14 @@
-test
+terraform {
+  backend "local" {
+    path = "test-terraform.tfstate"
+  }
+}
+
+
+provider "aws" {
+  region     = "us-east-1"
+  
+  assume_role {
+    role_arn = "arn:aws:iam::134425873773:role/EC2-Prod-Role"
+    }
+}
--- LaunchEC2/variables.tf
+++ LaunchEC2/variables.tf
@@ -1,4 +1,22 @@
-variable {
+variable "vpcid" {
+    default = "vpc-0bbf069330ac628d5"
+    description = "VPC iD"
+}
 
-    
+variable "subnet_id" {
+  default     = "subnet-0af82e914fc1a7ab3"
+}
+
+
+variable "app" {
+    type = string
+    description = "SAP Applications AMI"
+    default = "ami-04b70fa74e45c3917"
+}
+
+
+variable "instance_type_1" {
+   #type = string
+    default = "t2.micro"
+    description = "Instance Size web Server"
 }
